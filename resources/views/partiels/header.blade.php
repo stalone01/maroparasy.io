ba18bfe9f3ad91a0815ca305435f419afd34e645
stalone01 <rasoamampioninastalone@gmail.com>
2024-04-05 16:00:02 +0300

    stylisation de header avec branch master

--- resources/views/partiels/header.blade.php
+++ resources/views/partiels/header.blade.php
@@ -1,19 +1,23 @@
-<div class="bg-violet-950">
-    <div class="flex navbar">
-        <ul class="flex-row">
-            <li><a href="#">Acueil</a></li>
+<div class="bg-violet-950 h-1/5 text-white relative">
+    <div class="flex items-center h-12 static navbar">
+        <div class="ms-16 text-4xl font-extrabold">
+            <span class="bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-lime-400"">Stalone'S service</span>
+        </div>
+        <ul class="flex items-center space-x-10 ms-auto me-12 p-8 text-2xl">
+            <li><a href="#">Accueil</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">A propos</a></li>
             <li><a href="#">Contacts</a></li>
         </ul>
     </div>
-    <div class="IntroHead">
-        <div class="w-2/5 h-auto">
-            <h1>Lorem ipsum dolor sit amet.</h1>
+    <div class="flex items-center space-x-10 m-auto static me-10 ms-10 IntroHead">
+        <div class="w-1/2 h-auto mx-5">
+            <h1 class="text-4xl font-bold">Lorem ipsum dolor sit amet.</h1><br><br>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi pariatur neque libero doloremque veniam veritatis voluptatum et sed ipsa.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores soluta voluptate cum enim mollitia culpa?</p>
         </div>
-        <div class="w-1/3 h-auto">
-            <img src="/images/Atongona.jpg" alt="Rova Antongona" >
+        <div class="h-auto w-1/2 ">
+            <img src="/images/Atongona.jpg" alt="Rova Antongona" class="h-fit m-5 p-14 rounded-xl" >
         </div>
     </div>
 </div>

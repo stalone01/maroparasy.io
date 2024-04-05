47f5ad42de75631944bd3d121586c4119b009de0
stalone01 <rasoamampioninastalone@gmail.com>
2024-04-05 17:10:04 +0300

    ajout de 1er section de body

--- resources/views/page1.blade.php
+++ resources/views/page1.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div>
-        <h2>corps du page</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/>
-            Deserunt veniam illo doloremque incidunt nesciunt earum 
-            magnam sapiente possimus!</p>
+    <div class="bg-gray-200">
+    @include('partiels.section1')
     </div>
 @endsection
---
+++ resources/views/partiels/section1.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <h1 class="flex justify-center text-3xl text-violet-900 p-7 font-bold"><span>Lorem ipsum dolor sit, amet consectetur adipisicing elit</span>.</h1>
+    <div class="m-10">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, cum quos? Quisquam nemo voluptatem blanditiis placeat ratione eos quae natus! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et eum excepturi sit quis, aliquid fugiat at? Illo quam facilis impedit? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </p>
+    </div>
+    <div class="flex items-center space-x-5 m-8 ms-6 me-6">
+        <div class="bg-violet-500 p-5 shadow-lg shadow-gray-500 rounded-3xl">
+            <h2 class="flex justify-center p-2"><span>Lorem, ipsum.</span></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, tempore beatae! Minus, eligendi nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis doloribus labore aspernatur tempora molestias tempore suscipit sit ipsum autem nisi. </p>
+        </div>
+        <div class="bg-violet-500 p-5 shadow-lg shadow-gray-500 rounded-3xl">
+            <h2 class="flex justify-center p-2"><span>Lorem, ipsum.</span></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, tempore beatae! Minus, eligendi nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis doloribus labore aspernatur tempora molestias tempore suscipit sit ipsum autem nisi. </p>
+        </div>
+        <div class="bg-violet-500 p-5 shadow-lg shadow-gray-500 rounded-3xl">
+            <h2 class="flex justify-center p-2"><span>Lorem, ipsum.</span></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, tempore beatae! Minus, eligendi nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis doloribus labore aspernatur tempora molestias tempore suscipit sit ipsum autem nisi. </p>
+        </div>
+    </div>
+</div>

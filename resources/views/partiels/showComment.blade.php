e66cfe235727bca04ce0580b6706637d355a467a
stalone01 <rasoamampioninastalone@gmail.com>
2024-04-14 23:19:13 +0300

    fix: relation OneToOne OK et avatar-comment OK

--- resources/views/partiels/showComment.blade.php
+++ resources/views/partiels/showComment.blade.php
@@ -4,14 +4,15 @@
                 <h1 class="font-bold text-violet-950 text-2xl ">What Clients Say About our Experiences.</h1>
         </div>
         <div class="flex flex-col items-center justify-center ">
+            @foreach ($comments as $comment )
             <div class="flex items-center justify-center -mb-9 z-10">
                 <div class="bg-white w-16 h-16 border-2 border-violet-950 rounded-full animate-spin">
-                    <img class="" src="/images/avatar/profile-simple.svg" alt="profil">
+                    <img class="" src="{{ $comment->avatar->path }}" alt="profil">
                 </div>
             </div>
             <div class="grid-cols-1 w-3/5 h-42 border-8 border-gray-400 mb-28 p-4 pt-8 z-0">
                 <div>
-                @foreach ($comments as $comment )
+
                         <!-- on affiche le pseudo des clients et la date de leurs messages -->
                     <div class="info-profile">
                         <h3 class="flex justify-center font-semibold text-md ">{{$comment->username}}</h3>
@@ -22,7 +23,7 @@
                     <p class="flex justify-center bg-slate-100 rounded-lg p-2 mt-2">
                             {{$comment->msg}}
                     </p>
-                @endforeach
+            @endforeach
                 </div>
                 <div class="flex flex-row items-center justify-center mt-4">
                     {{ $comments->links('pagination::simple-tailwind') }}

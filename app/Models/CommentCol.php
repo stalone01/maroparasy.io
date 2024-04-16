d2a170346ac28e58acc20ae9eb428ffde40d4d7a
stalone01 <rasoamampioninastalone@gmail.com>
2024-04-16 09:43:26 +0300

    feat: mise en place de relation OneToMany

--- app/Models/CommentCol.php
+++ app/Models/CommentCol.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Colline;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CommentCol extends Model
 {
     use HasFactory;
+
+    public function colline(){
+        return $this->belongsTo(Colline::class);
+    }
 }

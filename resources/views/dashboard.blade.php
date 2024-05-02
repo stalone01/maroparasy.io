59a54d73caa13a16dc384457e976abc522439d03
stalone01 <rasoamampioninastalone@gmail.com>
2024-05-02 15:45:29 +0300

    fix: modification de dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, neque.</p>
         </h2>
     </x-slot>
 
@@ -9,7 +9,54 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+
+                    <div class="grid grid-cols-2 md:grid-cols-2 gap-3">
+                        <div class="h-auto max-w-full rounded-lg">
+                            <h1>Messages</h1>
+
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio consequuntur minima perspiciatis modi natus. Delectus, voluptate rem numquam suscipit totam explicabo id necessitatibus expedita vel neque ducimus molestias! Ullam, rerum!
+                            </p>
+                        </div>
+                        <div class="h-auto max-w-full rounded-lg">
+                            <h1>Utilisateurs</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, suscipit dolorem. Suscipit aspernatur numquam voluptatum at veniam velit alias enim rerum animi ea ducimus dolores esse excepturi, mollitia soluta, debitis nobis est autem. Saepe, id veritatis! Nam non perferendis in, ipsum similique asperiores inventore maxime dolorum vitae libero doloribus sit?
+                            </p>
+                        </div>
+                        <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+                        </div>
+                        <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+                        </div>
+                        <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
+                        </div>
+                        <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
+                        </div>
+                        {{-- <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="">
+                        </div> --}}
+                        {{-- <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="">
+                        </div> --}}
+                        {{-- <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="">
+                        </div> --}}
+                        {{-- <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="">
+                        </div> --}}
+                        {{-- <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="">
+                        </div> --}}
+                        {{-- <div>
+                            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="">
+                        </div> --}}
+                    </div>
+
                 </div>
             </div>
         </div>
